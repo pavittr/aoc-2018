c3d1bcdffe4aec9a3bf9f99874703f2b0f0eaa80
Ross Pavitt <rosspavitt@gmail.com>
2018-12-07 08:21:26 +0000

    Start of day7 (WIP)

---
+++ day7_test.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDay7Part1(t *testing.T) {
+
+	stepOrder := func(input string) string {
+
+		stepParser := regexp.MustCompile(`Step (?P<dependency>[[:upper:]]) must be finished before step (?P<stepLetter>[[:upper:]]) can begin.`)
+		dependencies := make(map[string][]string)
+		actionedSteps := make(map[string]bool)
+		for _, line := range strings.Split(input, "\n") {
+			fmt.Printf("Processing line %+v\n", line)
+
+			if !stepParser.MatchString(line) {
+				panic(fmt.Sprintf("Message is unreadable. line %+v", line))
+			}
+
+			groups := stepParser.FindStringSubmatch(line)
+			dependency := groups[1]
+			stepLetter := groups[2]
+			fmt.Printf("Line has dep %v and step %v\n", dependency, stepLetter)
+			stepDeps, alreadySeen := dependencies[stepLetter]
+			if !alreadySeen {
+				stepDeps = make([]string, 0)
+			}
+			stepDeps = append(stepDeps, dependency)
+			dependencies[stepLetter] = stepDeps
+			// Think this is unessessary?
+			actionedSteps[dependency] = false
+		}
+
+		for step, dependencies := range dependencies {
+			fmt.Printf("Step %v, dependencies: %+v\n", step, dependencies)
+		}
+		return ""
+	}
+	testInput := `Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.`
+
+	assert.Equal(t, "CABDFE", stepOrder(testInput))
+}
+
+func TestDay7Part2(t *testing.T) {
+}
+
+var day7Input = `Step Q must be finished before step O can begin.
+Step Z must be finished before step G can begin.
+Step W must be finished before step V can begin.
+Step C must be finished before step X can begin.
+Step O must be finished before step E can begin.
+Step K must be finished before step N can begin.
+Step P must be finished before step I can begin.
+Step X must be finished before step D can begin.
+Step N must be finished before step E can begin.
+Step F must be finished before step A can begin.
+Step U must be finished before step Y can begin.
+Step M must be finished before step H can begin.
+Step J must be finished before step B can begin.
+Step B must be finished before step E can begin.
+Step S must be finished before step L can begin.
+Step A must be finished before step L can begin.
+Step E must be finished before step L can begin.
+Step L must be finished before step G can begin.
+Step D must be finished before step I can begin.
+Step Y must be finished before step I can begin.
+Step I must be finished before step G can begin.
+Step G must be finished before step R can begin.
+Step V must be finished before step T can begin.
+Step R must be finished before step H can begin.
+Step H must be finished before step T can begin.
+Step S must be finished before step E can begin.
+Step C must be finished before step E can begin.
+Step P must be finished before step T can begin.
+Step I must be finished before step H can begin.
+Step O must be finished before step P can begin.
+Step M must be finished before step L can begin.
+Step S must be finished before step D can begin.
+Step P must be finished before step D can begin.
+Step P must be finished before step R can begin.
+Step I must be finished before step R can begin.
+Step Y must be finished before step G can begin.
+Step Q must be finished before step L can begin.
+Step N must be finished before step R can begin.
+Step J must be finished before step E can begin.
+Step N must be finished before step T can begin.
+Step B must be finished before step V can begin.
+Step Q must be finished before step B can begin.
+Step J must be finished before step H can begin.
+Step F must be finished before step B can begin.
+Step W must be finished before step X can begin.
+Step S must be finished before step T can begin.
+Step J must be finished before step G can begin.
+Step O must be finished before step R can begin.
+Step K must be finished before step B can begin.
+Step Z must be finished before step O can begin.
+Step Q must be finished before step S can begin.
+Step K must be finished before step V can begin.
+Step B must be finished before step R can begin.
+Step J must be finished before step T can begin.
+Step E must be finished before step T can begin.
+Step G must be finished before step V can begin.
+Step D must be finished before step Y can begin.
+Step M must be finished before step Y can begin.
+Step F must be finished before step G can begin.
+Step C must be finished before step P can begin.
+Step V must be finished before step R can begin.
+Step R must be finished before step T can begin.
+Step J must be finished before step Y can begin.
+Step U must be finished before step R can begin.
+Step Z must be finished before step F can begin.
+Step Q must be finished before step V can begin.
+Step U must be finished before step M can begin.
+Step J must be finished before step R can begin.
+Step L must be finished before step V can begin.
+Step W must be finished before step K can begin.
+Step B must be finished before step Y can begin.
+Step O must be finished before step N can begin.
+Step D must be finished before step V can begin.
+Step P must be finished before step B can begin.
+Step U must be finished before step I can begin.
+Step O must be finished before step T can begin.
+Step S must be finished before step G can begin.
+Step X must be finished before step A can begin.
+Step U must be finished before step T can begin.
+Step A must be finished before step I can begin.
+Step B must be finished before step G can begin.
+Step N must be finished before step Y can begin.
+Step Z must be finished before step J can begin.
+Step M must be finished before step D can begin.
+Step U must be finished before step A can begin.
+Step S must be finished before step R can begin.
+Step Z must be finished before step A can begin.
+Step Y must be finished before step R can begin.
+Step E must be finished before step Y can begin.
+Step N must be finished before step G can begin.
+Step Z must be finished before step X can begin.
+Step P must be finished before step X can begin.
+Step Z must be finished before step T can begin.
+Step Z must be finished before step P can begin.
+Step V must be finished before step H can begin.
+Step P must be finished before step L can begin.
+Step L must be finished before step H can begin.
+Step X must be finished before step V can begin.
+Step W must be finished before step G can begin.
+Step N must be finished before step D can begin.
+Step Z must be finished before step U can begin.`
